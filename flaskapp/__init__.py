cf328a0d7ba1b06173f449cd0ea59fed48f434fa
khanshifaul <khanshifaul@gmail.com>
2020-01-08 12:29:07 +0600

    proc improve

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -3,25 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from config import Config
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config.Development')
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-login_manager.category = 'info'
-login_manager.blueprint_login_views='auth.login'
+login_manager.login_message = _l('Please log in to access this page.')
+
+def create_app(congig_class):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object('config.Development')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    
 
 
-from flaskapp import public
-app.register_blueprint(public.bp)
+    from flaskapp import public
+    app.register_blueprint(public.bp)
 
-from flaskapp import auth
-app.register_blueprint(auth.auth)
-app.add_url_rule('/login', endpoint='index')
-
-# def create_db():
-#     db.create_all()
+    from flaskapp import auth
+    app.register_blueprint(auth.auth)
+    app.add_url_rule('/login', endpoint='index')
+    return app
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,8 @@
-from flaskapp import app, db
+from flaskapp import create_app, db
 from flaskapp.models import User, Student
 
-# @app.shell_context_processor
-# def make_shell_context():
-#     return {'db': db, 'User': User, 'Student': Student}
+app = create_app()
 
-if __name__ == '__main__':
-    app.run()
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Student': Student}
