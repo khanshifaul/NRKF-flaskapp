6dff167360cb795f828ae1e80fb73c77e29b731f
khanshifaul <khanshifaul@gmail.com>
2020-01-08 11:54:31 +0600

    modify

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,9 @@
 from flaskapp import app, db
 from flaskapp.models import User, Student
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Student': Student}
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Student': Student}
 
 if __name__ == '__main__':
     app.run
