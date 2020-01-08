0621192a5c71e11e366562d902c20dc07b95bb72
khanshifaul <khanshifaul@gmail.com>
2020-01-08 11:17:18 +0600

    correction

--- config.py
+++ config.py
@@ -10,11 +10,16 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    
 
 
 class Production(Config):
-    DATABASE_URI = 'postgresql://localhost/database'
+    DATABASE_URI = 'postgres://unzxbywmkcyuwt:112ab4fbbf7b5cb90b4799fb2e9b9ee146a5583194c6bda4a263c31f1882379c@ec2-174-129-33-88.compute-1.amazonaws.com:5432/d4886fqji3ve0k'
+
+class Staging(Config):
+    DEBUG = True
+    DATABASE_URI = 'postgres://unzxbywmkcyuwt:112ab4fbbf7b5cb90b4799fb2e9b9ee146a5583194c6bda4a263c31f1882379c@ec2-174-129-33-88.compute-1.amazonaws.com:5432/d4886fqji3ve0k'
+
 
 
 class Development(Config):
