2d6af19b0d0264423956928404bf40b799773485
khanshifaul <khanshifaul@gmail.com>
2020-01-08 14:13:41 +0600

    correction

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -5,28 +5,18 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from config import Config
 
-db = SQLAlchemy()
-migrate = Migrate()
-bcrypt = Bcrypt()
-login_manager = LoginManager()
+
+
+app = Flask(__name__,instance_relative_config=True)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 login_manager.login_view = 'auth.login'
 login_manager.login_message = ('Please log in to access this page.')
-
-def create_app(config_class=Config):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('config.Development')
-
-    db.init_app(app)
-    migrate.init_app(app, db)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    
-
-
-    from flaskapp import public
-    app.register_blueprint(public.bp)
-
-    from flaskapp import auth
-    app.register_blueprint(auth.auth)
-    app.add_url_rule('/login', endpoint='index')
-    return app
+from flaskapp import public
+app.register_blueprint(public.bp)
+from flaskapp import auth
+app.register_blueprint(auth.auth)
+app.add_url_rule('/login', endpoint='index')
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
-from flaskapp import create_app, db
+from flaskapp import app, db
 from flaskapp.models import User, Student
-
-app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
